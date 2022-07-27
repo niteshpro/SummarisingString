01d609f35236bc6b1e2a3e468be503c55a0e6aa2
niteshsharma9199 <niteshsharma9199@gmail.com>
2022-07-27 11:07:54 +0530

    source file code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var sentence = "This is a really really really really really really long semtence";
+    
+        Console.WriteLine(SummarizeText(sentence,20));
+        
+
+    }
+    static string SummarizeText(String text, int maxLength = 20)
+    {
+
+        if (text.Length < maxLength)
+            return text;
+        else
+        {
+            var words = text.Split(' ');
+            var totalCharacter = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacter += word.Length + 1;
+                if (totalCharacter > maxLength)
+                    break;
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+
+        }
+    }
+}
